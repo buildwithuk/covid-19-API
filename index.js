8dab68b09aeef90239965723268458b2ee956c79
Umair Khan <umairk83@live.com>
2025-05-01 15:43:36 +0500

    Fixed cors

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const history = require('./history');
 const statistics = require('./statistics');
-const countries = require('./countries');
+const countries = require('./countries'); 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://covid-19-dashboard-eight.vercel.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
